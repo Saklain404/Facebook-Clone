d3ce8051af4aedf84eccf75a7ec7ab8ce5225623
saklain404 <mohdsaklain8899@gmail.com>
2020-12-11 00:30:02 +0530

    Updates

---
+++ precache-manifest.d95a363b0f83fc199b944e42f183bf1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0943a89762fc902c8582ba80795bf9",
+    "url": "/Facebook-Clone/index.html"
+  },
+  {
+    "revision": "97a8b63cf0eef70a8376",
+    "url": "/Facebook-Clone/static/css/main.5bde1b8b.chunk.css"
+  },
+  {
+    "revision": "9efe989286acba119e61",
+    "url": "/Facebook-Clone/static/js/2.7ed78b32.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Facebook-Clone/static/js/2.7ed78b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a8b63cf0eef70a8376",
+    "url": "/Facebook-Clone/static/js/main.fab955d7.chunk.js"
+  },
+  {
+    "revision": "3fcf4e73c264ad2ddf2a",
+    "url": "/Facebook-Clone/static/js/runtime-main.35774d12.js"
+  },
+  {
+    "revision": "c679773f28c026bc6e5f9c475ec32a6f",
+    "url": "/Facebook-Clone/static/media/facebook.c679773f.png"
+  },
+  {
+    "revision": "33c2ecb06d27dc8262114d1568edc63c",
+    "url": "/Facebook-Clone/static/media/mine.33c2ecb0.jpeg"
+  },
+  {
+    "revision": "9effb8f506832f2aef53de5d644f881e",
+    "url": "/Facebook-Clone/static/media/naeem.9effb8f5.jpeg"
+  },
+  {
+    "revision": "fcf23bc26d93b8a5ab7cc1fe63f1f876",
+    "url": "/Facebook-Clone/static/media/pandit.fcf23bc2.jpeg"
+  },
+  {
+    "revision": "40618fcac744eb5d5841dca1f0626ff5",
+    "url": "/Facebook-Clone/static/media/panting.40618fca.jpeg"
+  },
+  {
+    "revision": "30adf7e685f00f3d3f2cfecc2d265905",
+    "url": "/Facebook-Clone/static/media/skln.30adf7e6.jpeg"
+  },
+  {
+    "revision": "a2226b1ab6cfcdceacace1bd89b5de80",
+    "url": "/Facebook-Clone/static/media/zaid.a2226b1a.jpeg"
+  }
+]);
